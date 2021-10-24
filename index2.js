e93e42d5042584166c3b3c02845e4d3c7761be8b
alexey popov <alexey.popov@5thkind.com>
2021-10-24 19:52:53 +0300

    update deserialize

--- index2.js
+++ index2.js
@@ -1,30 +1,41 @@
 const Schema = require("./employees_pb");
-const fs = require("fs")
+const fs = require("fs");
 
-const hussein = new Schema.Employee();
-hussein.setId(1001);
-hussein.setName("Hussein");
-hussein.setSalary(1001);
-
-const ahmed = new Schema.Employee();
-ahmed.setId(1002);
-ahmed.setName("Ahmed");
-ahmed.setSalary(9000);
-
-const rick = new Schema.Employee();
-rick.setId(1003);
-rick.setName("Rick");
-rick.setSalary(5000);
+const employee2Class = ({ id, name, salary }) => {
+  const employee = new Schema.Employee();
+  employee.setId(id);
+  employee.setName(name);
+  employee.setSalary(salary);
+  return employee;
+}
 
 const employees = new Schema.Employees();
-employees.addEmployees(hussein);
-employees.addEmployees(ahmed);
-employees.addEmployees(rick);
+[{
+  "name": "Hussein",
+  "salary": 1000,
+  "id": 1001
+}, {
+  "name": "Ahmed",
+  "salary": 9000,
+  "id": 1002
+}, {
+  "name": "Rick",
+  "salary": 5000,
+  "id": 1003
+}].map(item => employee2Class(item)).forEach(item => employees.addEmployees(item));
+
+
 const bytes = employees.serializeBinary();
 // console.log("binary " + bytes)
 fs.writeFileSync("employeesbinary", bytes)
 
-
 const employees2 = Schema.Employees.deserializeBinary(bytes);
+console.log(employees2.toObject().employeesList[0].id);
 
-console.log(JSON.stringify(employees2.array))
+// const ahmed = new Schema.Employee();
+// ahmed.setId(1002);
+// ahmed.setName("Ahmed");
+// ahmed.setSalary(9000);
+// const bytes = ahmed.serializeBinary();
+// const ahmed2 = Schema.Employee.deserializeBinary(bytes);
+// console.log(ahmed2.toObject());
